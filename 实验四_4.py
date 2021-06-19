9408377d9a9ead693e243fec80234e1dfabad299
xiangshane <84051027+xiangshane@users.noreply.github.com>
2021-06-19 19:27:38 +0800

    实验四

---
+++ 实验四_4.py
@@ -0,0 +1,18 @@
+from thinkdsp import Sinusoid
+from thinkdsp import normalize,unbias
+from thinkdsp import SquareSignal
+from thinkdsp import TriangleSignal
+from thinkdsp import decorate
+import matplotlib.pyplot as plt
+import numpy as np
+triangle = TriangleSignal().make_wave(duration=0.01)
+#triangle.plot()
+#decorate(xlabel='Time (s)')
+#plt.show()
+
+spectrum = triangle.make_spectrum()
+#print(spectrum.hs[0])
+spectrum.hs[0] = 100
+triangle.plot(color='gray')
+spectrum.make_wave().plot()
+plt.show()
